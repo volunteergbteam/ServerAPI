6b2b0a8f465b230593b4eb1d8cbf19b55f4f8f80
bazhenov4job <bazhenov4job@gmail.com>
2020-05-04 11:44:21 +0300

    Installed django, created project volunteerapi

---
+++ Volunteer/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/media/vadim/Локальный диск/PyProjects/Volunteer/ServerAPI/Volunteer/bin/python3.8
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
