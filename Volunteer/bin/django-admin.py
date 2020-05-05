b3ddd1e41ae5962e3574d65500760d2d2773c2f0
bazhenov4job <bazhenov4job@gmail.com>
2020-05-05 11:38:43 +0300

    added users app

--- Volunteer/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/media/vadim/Локальный диск/PyProjects/Volunteer/ServerAPI/Volunteer/bin/python3.8
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
