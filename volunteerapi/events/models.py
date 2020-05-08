5412885ae9a2c16da47b90a53f8c4891e5739a80
bazhenov4job <bazhenov4job@gmail.com>
2020-05-08 15:05:54 +0300

    added models to events and users

--- volunteerapi/events/models.py
+++ volunteerapi/events/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from users.models import User
 
 # Create your models here.
+
+
+class Cities(models.Model):
+
+    name = models.CharField(verbose_name="names", max_length=64, blank=False)
+
+
+class EventType(models.Model):
+
+    type = models.CharField(verbose_name='Event Types', max_length=64)
+    description = models.CharField(verbose_name='Descriptions', max_length=128)
+    avatar = models.ImageField(upload_to='uploads/', max_length=128)
+    rating = models.IntegerField()
+
+
+class Event(models.Model):
+
+    title = models.CharField(verbose_name='Titles', max_length=128)
+    creation_date = models.DateTimeField(auto_now=False, auto_now_add=True)
+    type_id = models.ForeignKey(EventType, verbose_name='Event Types')
+    description = models.CharField(verbose_name='Descriptions', max_length=256)
+    event_date = models.DateTimeField(verbose_name="Events Dates", auto_now=False, auto_now_add=False)
+    is_over = models.BooleanField()
+    owner_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    in_progress = models.BooleanField()
+    location_lon = models.FloatField()
+    location_lat = models.FloatField()
+    location_id = models.ForeignKey(Cities, on_delete=models.CASCADE)
--- volunteerapi/users/models.py
+++ volunteerapi/users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+
+    first_name = models.CharField(verbose_name="First Names", blank=False, max_length=64)
+    second_name = models.CharField(verbose_name="Second Names", blank=False, max_length=64)
