da89c52b89b4ef60056e87f0f05db213b39a5312
bazhenov4job <bazhenov4job@gmail.com>
2020-05-20 19:27:22 +0300

    added djoser for authentication and registration

--- volunteerapi/volunteerapi/settings.py
+++ volunteerapi/volunteerapi/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'djoser',
+    'rest_framework.authtoken',
     'events',
     'users',
 ]
